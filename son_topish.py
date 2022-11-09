9c271788f6b3f521a66aa1e5595dc0cb8b39edef
Mr-Khabibullayev <habibullayevaxrorbek960@gmail.com>
2022-11-09 13:31:05 +0500

    yuklandi

--- son_topish.py
+++ son_topish.py
@@ -28,46 +28,61 @@ while eng_katta_ishora:
             print(f"TOPDINGIZ!!! {oylagan_son} sonini o'ylagan edim. {len(qiymat_uzunligi)} ta taxmin bilan topdingiz.  Tabriklayman !!! ")
             ishora = False
             eng_katta_ishora = False
+        else:
+            print("Togri qiymat kiriting")
+            
+        
+    
+    eng_katta_ishora2 = True
+
+    while eng_katta_ishora2:
+        print("1 dan 10 gacha son oylang. Men topishga harakat qilaman.")
+        qiymat = input("Son O'ylagan bo'lsangiz enter tugmasini bosing.")
+        qiymat_uzunligi = []
+        sonlar = list(range(1,11))
+        # print(sonlar)
+        oylagan_sonim = r.choice(sonlar)
+
+        savol_sonning_qiyamti = str(input(f"Siz {oylagan_sonim} sonini o'yladingiz: to'g'rimi (T), men o'ylagan son     bundan kattaroq (+), yoki kichikroq (-) \n>> "))
+
+        ishora2 = True
+        while ishora2:
+            qiymat = savol_sonning_qiyamti
+            if qiymat  == '+':
+                oylagan_sonim += 1
+                qiymat_uzunligi2.append(qiymat)
+                savol_sonning_qiyamti = str(input(f"Siz {oylagan_sonim + 1} sonini o'yladingiz: to'g'rimi (T), men  o'ylagan son bundan kattaroq (+), yoki kichikroq (-) \n>> "))
+            elif qiymat == '-':
+                oylagan_sonim -= 1
+                qiymat_uzunligi2.append(qiymat)
+                savol_sonning_qiyamti = str(input(f"Siz {oylagan_sonim - 1} sonini o'yladingiz: to'g'rimi (T), men  o'ylagan son bundan kattaroq (+), yoki kichikroq (-) \n>> "))
+            elif qiymat == 't' or qiymat == 'T':
+                qiymat_uzunligi2.append(qiymat)
+                print(f"Soningizni {len(qiymat_uzunligi2)} ta taxmin bilan topdik")
+                ishora2 = False
+                eng_katta_ishora2 = False
+            else:
+                print("Togri qiymat kiriting")
+
+
+
+
+    if qiymat_uzunligi > qiymat_uzunligi2:
+        print(f"Siz {len(qiymat_uzunligi2)} ta taxmin bilan topdingiz va yutqazdingiz ")
+    elif qiymat_uzunligi2 > qiymat_uzunligi:
+        print(f"Siz {len(qiymat_uzunligi2)} ta taxmin bilan topdingiz va yutdingiz ")
+    elif qiymat_uzunligi2 == qiymat_uzunligi:
+        print(f"Siz {len(qiymat_uzunligi2)} ta taxmin bilan topdingiz va durrang! ")
+
+    savol = str(input("Yana o'ynashni hohlaysizmi ? ha(h)  /  yo'q(y) \n>> "))
+    if savol == 'h':
+        eng_katta_ishora = True
+        eng_katta_ishora2 = True
+        ishora = True
+        ishora2= True
+    else :
+        print("O'yinni yakunladingiz")
         
    
     
     
-eng_katta_ishora2 = True
-
-while eng_katta_ishora2:
-    print("1 dan 10 gacha son oylang. Men topishga harakat qilaman.")
-    qiymat = input("Son O'ylagan bo'lsangiz enter tugmasini bosing.")
-    qiymat_uzunligi = []
-    sonlar = list(range(1,11))
-    # print(sonlar)
-    oylagan_sonim = r.choice(sonlar)
-    
-    savol_sonning_qiyamti = str(input(f"Siz {oylagan_sonim} sonini o'yladingiz: to'g'rimi (T), men o'ylagan son bundan kattaroq (+), yoki kichikroq (-) \n>> "))
-    
-    ishora2 = True
-    while ishora2:
-        qiymat = savol_sonning_qiyamti
-        if qiymat  == '+':
-            oylagan_sonim += 1
-            qiymat_uzunligi2.append(qiymat)
-            savol_sonning_qiyamti = str(input(f"Siz {oylagan_sonim + 1} sonini o'yladingiz: to'g'rimi (T), men o'ylagan son bundan kattaroq (+), yoki kichikroq (-) \n>> "))
-        elif qiymat == '-':
-            oylagan_sonim -= 1
-            qiymat_uzunligi2.append(qiymat)
-            savol_sonning_qiyamti = str(input(f"Siz {oylagan_sonim - 1} sonini o'yladingiz: to'g'rimi (T), men o'ylagan son bundan kattaroq (+), yoki kichikroq (-) \n>> "))
-        elif qiymat == 't' or qiymat == 'T':
-            qiymat_uzunligi2.append(qiymat)
-            print(f"Soningizni {len(qiymat_uzunligi2)} ta taxmin bilan topdik")
-            ishora2 = False
-            eng_katta_ishora2 = False
-            
-
-if qiymat_uzunligi > qiymat_uzunligi2:
-    print(f"Siz {len(qiymat_uzunligi2)} ta taxmin bilan topdingiz va yutqazdingiz ")
-elif qiymat_uzunligi2 > qiymat_uzunligi:
-    print(f"Siz {len(qiymat_uzunligi2)} ta taxmin bilan topdingiz va yutdingiz ")
-
-savol = int(input("Yana o'ynashni hohlaysizmi ? ha(1)  /  yo'q(0) \n>> "))
-if savol == 1:
-    eng_katta_ishora = True
-    eng_katta_ishora2 = True
